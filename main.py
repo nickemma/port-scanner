96ec95f7b6c15b19479e10f905dc790e81042539
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-04-03 22:59:26 +0100

    Asked the user for ip adresses to scan and the port splitter by comma

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('hello word')
+import socket
+import termcolor
+
+def scan(target, ports):
+    for port in range(1, ports):
+        scan_port(target, port)
+
+# Set the IP address and port number of the server to connect to in a function
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print(termcolor.colored("[+] Port Opened ", "green" + str(port)))
+        socket.close()
+    except:
+        print(termcolor.colored("[-] Port Closed ", "red" + str(port)))
+
+        
+targets = input("[*] Enter Targets To Scan (split them by ,): ")
+ports = input("[*] Enter How Many Ports You Want To Scan: ")
+
+if "," in targets:
+    print(termcolor.colored("[*] Scanning Multiple Targets", "blue"))
+    for ip_addr in targets.split(","):
+        scan(ip_addr.strip(" "), ports)
+else:
+    scan(targets, ports)
